25056729ba60abc1f0c272d108fec1ecd2c99c55
mackings <kingsleyudoma2018@gmail.com>
2022-10-07 15:49:54 +0100

    post module

---
+++ database/db.js
@@ -0,0 +1,9 @@
+ const mongoose = require('mongoose');
+
+ const Task = mongoose.Schema({
+    title:String,
+    description:String
+ })
+
+
+ module.exports = ("Task", mongoose);
--- index.js
+++ index.js
@@ -3,12 +3,26 @@ const app = express();
 const {v4: uuid } = require("uuid");
 const fs = require("fs/promises");
 require ("dotenv").config;
+const Task = require("./database/db");
 
 app.use(express.urlencoded({extended:true}));
 app.use(express.json());
 
+const posts = [];
+
+
 app.get("/",function(req,res){
     res.send("Server is up and Running");
+});
+
+app.post("/addpost", async function(req,res){
+
+    await  posts.push(req.body);
+});
+
+
+app.get("/allpost", async function(req,res){
+    res.send(posts);
 });
 
 
@@ -35,3 +49,5 @@ app.get("/names/:id",function(req,res){
 
 const port = process.env.PORT || 5000
 app.listen(port); 
+
+
